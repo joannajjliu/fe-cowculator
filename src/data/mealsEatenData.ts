578b2a9a6ec25e3048ddfcd754066196c4349457
joannajjliu <joanna.liu3774@gmail.com>
2021-04-24 20:42:08 -0400

    mock api contract

---
+++ src/data/mealsEatenData.ts
@@ -0,0 +1,72 @@
+export const mealsEatenData = [
+  {
+    mealId: 6,
+    dateUploaded: '2021-04-23T17:02:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/s240x240/174611984_888720555038403_67130674708762196_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=XIfO4-g_rokAX9KZ0GN&edm=AFH1aioAAAAA&ccb=7-4&oh=39c2cd51a0ad563e43193bf61fcb3781&oe=60A9697D&_nc_sid=227b6c&ig_cache_key=MjU1NTQ0Njk5MzMxMDAyNTQ2MA%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'cucumbers', carbonEquivalent: '2.0' },
+      { item: 'jam', carbonEquivalent: '3.0' },
+      { item: 'egg', carbonEquivalent: '4.8' },
+      { item: 'ham', carbonEquivalent: '12.1' },
+    ],
+  },
+  {
+    mealId: 5,
+    dateUploaded: '2021-04-23T09:05:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/p240x240/168689430_122982739800763_8281290267516279657_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=107&_nc_ohc=Fr1xJy1mgpsAX-XQqs0&edm=AFH1aioAAAAA&ccb=7-4&oh=75cb81fe6589b0ad9597bace85edfe03&oe=60A8B12A&_nc_sid=227b6c&ig_cache_key=MjU0Mzg2OTIzNzQwNDEzNDAzNQ%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'wheat', carbonEquivalent: '2.5' },
+      { item: 'strawberries', carbonEquivalent: '1.1' },
+      { item: 'banana', carbonEquivalent: '1.1' },
+      { item: 'milk', carbonEquivalent: '1.9' },
+    ],
+  },
+  {
+    mealId: 4,
+    dateUploaded: '2021-04-22T15:12:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/p240x240/176026460_737765620234347_8029354371300885223_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=nILscVOk43sAX_73qbL&edm=AFH1aioAAAAA&ccb=7-4&oh=3e4726797b08727bf79f89a07da6a30a&oe=60AA48C1&_nc_sid=227b6c&ig_cache_key=MjU1Njc3MTA5NzE0OTU5MjIwMg%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'cucumber', carbonEquivalent: '2.0' },
+      { item: 'tomatoes', carbonEquivalent: '2.0' },
+      { item: 'cheese', carbonEquivalent: '13.5' },
+      { item: 'ham', carbonEquivalent: '12.1' },
+      { item: 'olives', carbonEquivalent: '3.1' },
+    ],
+  },
+  {
+    mealId: 3,
+    dateUploaded: '2021-04-22T10:41:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/p240x240/169702576_5827924967221452_9106497711537611029_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=SOEiBhYhwRgAX_X3FBs&edm=AFH1aioAAAAA&ccb=7-4&oh=896f79267c1166483ed3c56997144510&oe=60AAF088&_nc_sid=227b6c&ig_cache_key=MjU0NTIyOTYyNjcwNDQ3MTAyNQ%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'flour', carbonEquivalent: '2.5' },
+      { item: 'strawberries', carbonEquivalent: '1.1' },
+      { item: 'blueberries', carbonEquivalent: '1.1' },
+    ],
+  },
+  {
+    mealId: 2,
+    dateUploaded: '2021-04-21T16:22:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/s240x240/176353893_1094233151085736_780100073323127689_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=MdH57TQybU8AX_0lrfa&edm=AFH1aioAAAAA&ccb=7-4&oh=d58c8994508640bc2ace0a8eb03d0342&oe=60A89FB7&_nc_sid=227b6c&ig_cache_key=MjU1ODI2NTMyNzg4MDcyNTA1Mw%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'flour', carbonEquivalent: '2.5' },
+      { item: 'rice', carbonEquivalent: '2.7' },
+      { item: 'beans', carbonEquivalent: '2.0' },
+    ],
+  },
+  {
+    mealId: 1,
+    dateUploaded: '2021-04-21T09:31:36.386Z',
+    imgUrl:
+      'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/p240x240/176554177_292706565908715_2747246993403542680_n.jpg?tp=1&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=VAqErepoQrMAX9zP__O&edm=AFH1aioAAAAA&ccb=7-4&oh=86f57da46b60af76dcf0790f5c2727cb&oe=60AA043D&_nc_sid=227b6c&ig_cache_key=MjU1OTE4MjQxODc4MzUzNTgzNw%3D%3D.2-ccb7-4',
+    ingredients: [
+      { item: 'rasberries', carbonEquivalent: '1.1' },
+      { item: 'blueberries', carbonEquivalent: '1.1' },
+      { item: 'flour', carbonEquivalent: '2.5' },
+    ],
+  },
+];
